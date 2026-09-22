a1dfbc1f64912bc533a0b05633d7539902ebb194
Armando Betancourt <armabetancourtt@gmail.com>
2026-09-21 20:31:50 -0600

    feat: add native iOS and Android foundations

---
+++ ios/PixelGo/KeychainStore.swift
@@ -0,0 +1,38 @@
+import Foundation
+import Security
+
+struct KeychainStore {
+    enum KeychainError: Error { case unhandled(OSStatus) }
+
+    func save(_ data: Data, account: String) throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: "com.armaabetancourtt.pixelgo",
+            kSecAttrAccount as String: account
+        ]
+
+        SecItemDelete(query as CFDictionary)
+        var insert = query
+        insert[kSecValueData as String] = data
+        insert[kSecAttrAccessible as String] = kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
+
+        let status = SecItemAdd(insert as CFDictionary, nil)
+        guard status == errSecSuccess else { throw KeychainError.unhandled(status) }
+    }
+
+    func read(account: String) throws -> Data? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: "com.armaabetancourtt.pixelgo",
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        if status == errSecItemNotFound { return nil }
+        guard status == errSecSuccess else { throw KeychainError.unhandled(status) }
+        return item as? Data
+    }
+}
